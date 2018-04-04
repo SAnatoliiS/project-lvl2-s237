b4fe8d7b79e05a5c590f802dc816aa8f1864d109
Anatolii Sergeev <serg.anatolii.serg@gmail.com>
2018-04-05 00:12:19 +0300

    fix mastakes

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -6,7 +6,7 @@ const example = fs.readFileSync('./__tests__/__fixtures__/result', 'utf-8');
 
 test('gendiff', () => {
   expect(gendiff(
-    '/home/anatolii/Project2/__tests__/__fixtures__/before.json',
-    '/home/anatolii/Project2/__tests__/__fixtures__/after.json',
+    './__tests__/__fixtures__/before.json',
+    './__tests__/__fixtures__/after.json',
   )).toBe(example);
 });
