98782df85bdd581d297f450f10ea02f5efb093c8
Anatolii Sergeev <serg.anatolii.serg@gmail.com>
2018-04-05 19:11:26 +0300

    fix mistakes

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,9 +2,8 @@ import fs from 'fs';
 import gendiff from '../src';
 
 
-const example = fs.readFileSync('./__tests__/__fixtures__/result', 'utf-8');
-
 test('gendiff(JSON)', () => {
+  const example = fs.readFileSync('./__tests__/__fixtures__/result', 'utf-8');
   expect(gendiff(
     './__tests__/__fixtures__/before.json',
     './__tests__/__fixtures__/after.json',
@@ -12,6 +11,7 @@ test('gendiff(JSON)', () => {
 });
 
 test('gendiff(YAML)', () => {
+  const example = fs.readFileSync('./__tests__/__fixtures__/result', 'utf-8');
   expect(gendiff(
     './__tests__/__fixtures__/before.yaml',
     './__tests__/__fixtures__/after.yaml',
--- src/genDiff.js
+++ src/genDiff.js
@@ -9,31 +9,36 @@ const parsers = {
   '.yml': safeLoad,
 };
 
-const getObject = (config) => {
+const fileReader = filePath => fs.readFileSync(filePath, 'UTF-8');
+
+const getParser = extractor => (config) => {
   const ext = path.extname(config);
-  const configAsObj = parsers[ext](fs.readFileSync(config));
-  if (!configAsObj) throw new Error(`unkown format: ${ext}`);
-  return configAsObj;
+  const configParsed = parsers[ext](extractor(config));
+  if (!configParsed) throw new Error(`unkown format: ${ext}`);
+  return configParsed;
 };
 
 export default function gendiff(firstConfig, secondConfig) {
-  const firstConfigAsObj = getObject(firstConfig);
-  const secondConfigAsObj = getObject(secondConfig);
+  const parser = getParser(fileReader);
 
-  const preResult = Object.keys(firstConfigAsObj).reduce((acc, key) => {
-    const firstValue = firstConfigAsObj[key];
-    const secondValue = secondConfigAsObj[key];
-    if (_.has(secondConfigAsObj, key)) {
-      if (firstValue === secondValue) {
-        return `${acc}\n    ${key}: ${firstValue}`;
+  const firstConfigParsed = parser(firstConfig);
+  const secondConfigParsed = parser(secondConfig);
+
+  const preResult = _.union(Object.keys(firstConfigParsed), Object.keys(secondConfigParsed))
+    .reduce((acc, key) => {
+      const firstValue = firstConfigParsed[key];
+      const secondValue = secondConfigParsed[key];
+      if (_.has(firstConfigParsed, key)) {
+        if (_.has(secondConfigParsed, key)) {
+          if (firstValue === secondValue) {
+            return `${acc}\n    ${key}: ${firstValue}`;
+          }
+          return `${acc}\n  + ${key}: ${secondValue}\n  - ${key}: ${firstValue}`;
+        }
+        return `${acc}\n  - ${key}: ${firstValue}`;
       }
-      return `${acc}\n  + ${key}: ${secondValue}\n  - ${key}: ${firstValue}`;
-    }
-    return `${acc}\n  - ${key}: ${firstValue}`;
-  }, '{');
-  const result = Object.keys(secondConfigAsObj).reduce((acc, key) => {
-    if (_.has(firstConfigAsObj, key)) return acc;
-    return `${acc}\n  + ${key}: ${secondConfigAsObj[key]}`;
-  }, preResult);
-  return `${result}\n}`;
+      return `${acc}\n  + ${key}: ${secondValue}`;
+    }, '{');
+  const result = `${preResult}\n}`;
+  return result;
 }
